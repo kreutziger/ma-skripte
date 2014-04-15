8f68e3fe0fe278c59f513a33070c3d6a4f30b461
knrkstrr <knrkstrr@gmail.com>
2014-04-15 09:30:26 +0200

    didnt test /:

--- TimeTableMaker.py
+++ TimeTableMaker.py
@@ -15,7 +15,7 @@ class TimeTableMaker:
         self.str_arr = self.time_string.split(';')
         if len(self.str_arr) != 3:
             self.help_message()
-            sys.exit(1)
+            sys.exit(3)
         tmp = self.str_arr[0].split(':')
         self.start = (int(tmp[0]), int(tmp[1]))
         self.pause = int(self.str_arr[1])
@@ -86,8 +86,8 @@ class TimeTableMaker:
         print(self.result)
 
 if __name__ == '__main__':
-    if (len(sys.argv) < 3):
+    if (len(sys.argv) < 2):
         TimeTableMaker.help_message()
-        sys.exit(0)
+        sys.exit(2)
     ttm = TimeTableMaker(sys.argv[1])
     ttm.print_time_table()
